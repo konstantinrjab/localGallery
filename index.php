1311de3370b6b217f4dc050f69ead6719649e6f3
konstantinrjab <konstantinrjab@gmail.com>
2018-09-22 13:23:51 +0300

    carousel deleted

--- index.php
+++ index.php
@@ -10,11 +10,7 @@ if (isset($_POST['password']) ||
 $path = 'img/';
 if (is_dir($path)) {
     $images = glob("$path*.*");
-
-    //$images = array_slice($images, 5, 30);
-    //echo '<pre>';
-    //print_r($images);
-    //echo '</pre>';
+    shuffle($images);
 }
 ?>
 <!doctype html>
@@ -24,13 +20,10 @@ if (is_dir($path)) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/8.15.2/lazyload.min.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
 
     <title>Gallery</title>
 </head>
@@ -56,32 +49,11 @@ if (is_dir($path)) {
     </div>
 <?php else : ?>
     <?php if (isset($images)) : ?>
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-
-            <div class="carousel-inner">
-
-                <?php foreach ($images as $i => $image) : ?>
-                    <div class="carousel-item">
-                        <img id="<?= $i ?>_carousel" class="d-block carousel-item-child" src="<?= $image; ?>">
-                    </div>
-                <?php endforeach; ?>
-
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-
         <div class="container mt-4 gallery">
             <div class="row">
                 <?php foreach ($images as $i => $image) : ?>
                     <div class="col-sm-6 col-lg-3">
-                        <img id="<?= $i ?>" class="d-block w-100" src="<?= $image; ?>">
+                        <img id="<?= $i ?>" class="d-block w-100 lazy" src="<?= $image; ?>">
                     </div>
                 <?php endforeach; ?>
             </div>
--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-$(document).ready(function () {
-
-	$('.carousel').carousel({
-	    interval: false,
-	    pause: "false"
-	});
-
-	$('.gallery img').click(function () {
-	    $('.active').removeClass('active');
-	    $('#'+this.id+'_carousel').closest('div').addClass('active');
-	    window.scrollTo(0, 0);
-	})
+new LazyLoad({
+    elements_selector: ".lazy"
 });
